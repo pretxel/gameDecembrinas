04db6978bcac0e6fa3bcd3d7f46ddfda0ce3ee79
Edsel Serrano <pretxel@hotmail.com>
2014-11-03 19:12:10 -0600

    init Repo

---
+++ js/resources.js
@@ -0,0 +1,16 @@
+game.resources = [
+
+    { name: "lower_floor", type: "image", src: "data/img/lower_floor.png" },
+    { name: "upper_floor", type: "image", src: "data/img/upper_floor.png" },
+    { name: "metatiles32x32",  type:"image", src: "data/map/metatiles32x32.png"},
+
+    { name: "gripe", type: "image", src: "data/sprite/gripe.png" },
+
+    { name: "map1", type: "tmx", src: "data/map/1.tmx" }
+    // { name: "map2", type: "tmx", src: "data/map/2.xml" },
+    // { name: "map3", type: "tmx", src: "data/map/3.xml" },
+
+
+ 
+
+];
